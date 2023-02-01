ee4dd439e6d82dc7f3f54c988a901c8fc0e2adbd
Trung Dong Huynh <trungdong@donggiang.com>
2023-02-01 17:43:18 +0000

    Connected to the local provman and log2prov

--- __init__.py
+++ __init__.py
@@ -1,15 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+This package provides prov-auditor-skill for Mycroft.
+Author: Trung Dong Huynh
+"""
+import logging
+from pathlib import Path
+
 from mycroft import MycroftSkill, intent_file_handler
+from .skill.provtools import provman_narrate_batch, log2prov
 
 
 class ProvAuditor(MycroftSkill):
+    PROVENANCE_SAMPLE_FILEPATH = Path("/home/tdh/projects/explanations/sais/provenance/sim.provn")
+    NARRATIVE_PROFILE = "ln:company-pronoun-1,ln:borrower-person2"
+
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super().__init__("ProvAuditor")
+
+    def initialize(self):
+        pass
 
     @intent_file_handler('auditor.prov.intent')
     def handle_auditor_prov(self, message):
-        self.speak_dialog('auditor.prov')
+        sentences = self.generate_narratives()
+        for s in sentences:
+            self.speak_dialog(s)
+
+    def generate_narratives(self) -> list[str]:
+        provn = self.expand_provenance()
+        xplan = "sais.user-data-sum"
+        self.log.debug("Generating narratives for the following explanation plan: %s", xplan)
+        narratives = provman_narrate_batch(provn, [xplan], self.NARRATIVE_PROFILE)
+        # returning a list of separate sentences
+        sentences = list(filter(len, map(str.strip, narratives[xplan].split("\n\n"))))
+        self.log.debug("-> %d sentences generated", len(sentences))
+        return sentences
+
+    def sample_bindings(self) -> list[str]:
+        return [
+            'user_datapoint,users/3259,users/3259/ip,UserIPAddress,51.83.74.23',
+            'user_datapoint,users/3259,users/3259/geolocation,UserGeoLocation,"45.47885,133.42825"',
+            'user_datapoint,users/3259,users/3259/city,UserLocation,"Lesozavodsk, RU"',
+            'user_datapoint,users/3259,users/3259/timezone,UserTimezone,Asia/Vladivostok',
+            'intent_matching,users/3259,mycroft,utterance/2355,"hey mycroft, weather forecast",intent/6993,,mycroft-weather,{intent: current-weather},2023-01-12T07:22:17.153345',
+            'skill_invocation,mycroft-weather,openweathermap.org,intent/6993,users/3259/ip,users/3259/geolocation,req/de473ad391304fb2b634307dc7db6264,APIRequest,,2023-01-12T07:22:17.200082,res/b3121142a3ab448995eac3321b8c5c19,APIResponse,,2023-01-12T07:22:17.271877,act/16cc16ed9cdd4a019773103bffb19f49',
+        ]
+
+    def expand_provenance(self) -> str:
+        bindings = self.sample_bindings()
+        self.log.debug("Expanding provenance from %d CSV bindings", len(bindings))
+        return log2prov(bindings)
 
 
 def create_skill():
     return ProvAuditor()
-
---
+++ skill/provtools.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+This package provides an interface to command-line tools.
+Author: Trung Dong Huynh
+"""
+import json
+import logging
+import os
+import subprocess
+import tempfile
+from typing import Sequence
+
+
+PROVCONVERT_PATH = "provconvert"
+PROVMAN_PATH = "provmanagement"
+
+logger = logging.getLogger(__name__)
+
+
+def call_external_tool(executable, arguments, pipe_input=None, timeout=None, env=None) -> str:
+    """Call the external command-line tool at `executable` with the provided arguments.
+    Args:
+        executable:
+        arguments:
+        pipe_input:
+        timeout:
+    Returns: The output from the execution of the tool.
+    """
+    args = list(map(str, (executable, *arguments)))
+    logger.debug("Calling command: %s", " ".join(args))
+    p = subprocess.Popen(
+        args,
+        stdin=subprocess.PIPE if pipe_input is not None else None,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        env=env,
+    )
+    b_input = pipe_input.encode("utf8") if pipe_input is not None else None
+    stdout, stderr = p.communicate(input=b_input, timeout=timeout)
+    if p.returncode != 0:
+        stdout, stderr = stdout.decode(), stderr.decode()
+        logger.debug(
+            "%s returns non-zero code (%d)\nOutput: %s\nError: %s",
+            executable,
+            p.returncode,
+            stdout,
+            stderr,
+        )
+        raise subprocess.CalledProcessError(p.returncode, args, stdout, stderr)
+    return stdout.decode()
+
+
+def provman_narrate_batch(provn: str, templates: Sequence[str], profile: str = None) -> dict:
+    """Explain command specific to the credit scoring scenario.
+    """
+    batch_templates = ",".join(f'[{template}]' for template in templates)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        output_filepath = os.path.join(tmpdirname, "narrative.provn")
+        narrative_filepath = os.path.join(tmpdirname, "narrative.json")
+        arguments = [
+            "explain",
+            "--infile", "-",
+            "--outfile", output_filepath,
+            "--text", narrative_filepath,
+            # -s $(OUTPUTS)/narrative-simplenlg.txt
+            "--language", "/home/tdh/projects/explanations/sais/xplan/sais-template-library.json",
+            f"--batch-templates={batch_templates}",
+            "-X", "0",  # do not include HTML mark-ups
+        ]
+        if profile is not None:
+            arguments.append("--profile=" + profile)
+
+        call_external_tool(PROVMAN_PATH, arguments, pipe_input=provn)
+
+        with open(narrative_filepath) as f:
+            return json.load(f)
+
+
+def log2prov(bindings: Sequence[str]) -> str:
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        os.environ["CLASSPATH_PREFIX"] = "/home/tdh/.m2/repository/org/openprovenance/sais/templates_l2p/0.1.0/templates_l2p-0.1.0.jar:/home/tdh/.m2/repository/org/openprovenance/sais/templates_cli/0.1.0/templates_cli-0.1.0.jar"
+        output_filepath = os.path.join(tmpdirname, "expansion.provn")
+        bindings_str = "\n".join(bindings)
+        arguments = [
+            "--infile", "-",
+            "--log2prov", "org.openprovenance.sais.Init",
+            "--outfile", output_filepath,
+        ]
+
+        call_external_tool(PROVCONVERT_PATH, arguments, pipe_input=bindings_str, env=os.environ)
+
+        with open(output_filepath) as f:
+            return f.read()
